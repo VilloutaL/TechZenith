34467a994583bd8592c084c22c835b689c2aa12d
SalbornozD <saad11012002@gmail.com>
2024-06-28 22:46:23 -0400

    SCRUM-30 Creado Modelo de Usuario, Token

--- aula_virtual/models.py
+++ aula_virtual/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class Usuario(AbstractUser):
+    tutor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='alumnos')
+
+    def __str__(self):
+        if self.first_name == "":
+            return self.username
+        return self.first_name
+
+class Token(models.Model):
+    token_autenticacion = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dead_time = models.DateTimeField()
+    ID_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def is_expired(self):
+        return timezone.now() > self.dead_time
+
+    def __str__(self):
+        return str(self.token_autenticacion)
