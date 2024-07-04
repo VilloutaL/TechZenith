9d1729f4e5feb896cfb36bd26c1f29fd60d0b2f2
SalbornozD <saad11012002@gmail.com>
2024-07-04 00:43:49 -0400

    HU 16: Justificacion de inasistencia - Terminada

--- aula_virtual/urls.py
+++ aula_virtual/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
     path('obtener-usuarios/', views.obtener_usuarios), #JSON
     path('obtener_usuario/', views.obtener_usuario), #JSON
     path("", views.index, name="index"),
+    path('logout/', views.logout_view, name='logout'),
+    path("home/", views.home, name="home"),
     path('nuevo-usuario/', views.nuevo_usuario, name='nuevo_usuario'),
     path('configuracion-cuenta/<uuid:token>/', views.configuracion_cuenta, name='configuracion-cuenta'),
+    path('justificar/<str:rut_alumno>', views.justificar, name="justificar"),
     path("asignaturas/", views.mis_asignaturas, name= 'mis_asignaturas'),
 ]
+
